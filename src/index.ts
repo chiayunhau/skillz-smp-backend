118085318ab79fd2f63c64c24542dff14adfe621
chiayunhau <hauchiayun@gmail.com>
2023-06-30 15:18:58 +0800

    Initial Commit

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,18 @@
 import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
-import { error } from 'console';
 
 const app = express();
 app.use(bodyParser.json());
 
 app.get('/api/users', (req: Request, res: Response) => {
     try{
-
+        
     }catch(error){
+        console.log(error)
         res.status(500).json(error)
     }
 });
 
-app.post('/api/users', (req: Request, res: Response) => {
-  // Handle POST request for /api/users
-  const { name } = req.body;
-  const newUser = { id: 3, name };
-  res.json(newUser);
-});
 
 const port = 3000;
 app.listen(port, () => {
